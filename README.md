876a7a434756b149b93b93c80ef2861d7773aa0a
jacky841224j <jacky841224j@gmail.com>
2020-10-28 17:47:04 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# RSSAPP
+# tg-rss-bot
+
+中文 Telegram RSS 机器人 [@TheBlackRSSBot](https://telegram.me/TheBlackRSSBot)
+
+（ID 都被占用了，所以随便起的）
+
+## 使用
+
+    /rss   - 显示当前订阅的 RSS 列表，加 raw 参数显示链接
+    /sub   - 订阅一个 RSS: /sub http://example.com/feed.xml
+    /unsub - 退订一个 RSS: /unsub http://example.com/feed.xml
+
+## 编译
+
+**依赖：**
+- JRE or JDK >= 1.6
+- [Leiningen](https://leiningen.org/)
+
+```
+git clone https://github.com/iovxw/tg-rss-bot.git
+cd tg-rss-bot
+lein uberjar
+```
+
+编译好的文件在 `./target/uberjar/tg-rss-bot-0.5.9-standalone.jar`
+
+## 运行
+
+**依赖：**
+- JRE or JDK >= 1.6
+
+```
+java -jar tg-rss-bot-0.5.9-standalone.jar [bot-token]
+```
+
+`bot-token` 的申请请参照 <https://core.telegram.org/bots#creating-a-new-bot>
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2016 iovxw
---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject tg-rss-bot "0.5.9"
+  :description "Yet another RSS bot for Telegram"
+  :url "https://github.com/iovxw/tg-rss-bot"
+  :license {:name "The MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "3.1.0"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojars.dive-networks/feedparser-clj "0.5.3"]]
+  :main ^:skip-aot tg-rss-bot.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+
